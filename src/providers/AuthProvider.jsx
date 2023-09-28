1d8a505603d387ec5241e22f6ea8082ea9217a92
arvndblocker"nn <aravindvasang@gmail.com>
2023-09-28 19:37:00 +0530

    blog_app

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,9 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({children}) => {
-    return (
-        <SessionProvider>{children}</SessionProvider>
-    )
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
 }
 
 export default AuthProvider
